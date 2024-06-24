f9043008c727022d1a31fccc00587f90a60e49d9
Peyman Zarchini <peymanzarchini@outlook.com>
2024-06-24 20:36:31 +0330

    Creating the design of the header part of the project

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between py-4 px-6 container mx-auto">
+      <Link href={"/"} className="font-bold text-xl">
+        Job Board
+      </Link>
+      <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-md">
+        <Link href={"/login"} className="bg-gray-200">
+          Login
+        </Link>
+        <Link href={"new-listing"} className="bg-blue-600 text-white">
+          Post a job
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
