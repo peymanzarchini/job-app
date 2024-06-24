f6c5658c15f33bc7a07494978e110ac8db65f7ec
Peyman Zarchini <peymanzarchini@outlook.com>
2024-06-24 21:24:26 +0330

    Creating the design of the hero part of the project

---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <section className="py-12">
+      <h1 className="text-4xl text-center font-bold">
+        Find your next
+        <br />
+        dream job
+      </h1>
+      <form className="mt-5 flex gap-2 max-w-lg mx-auto">
+        <input
+          type="text"
+          placeholder="Search phrase"
+          className="border border-gray-400 w-full rounded-md py-2 px-3"
+        />
+        <button className="bg-blue-600 text-white py-2 px-4 rounded-md">Search</button>
+      </form>
+    </section>
+  );
+};
+
+export default Hero;
