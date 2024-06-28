668d090575cd53d952def6618e6d5a9c72821676
Peyman Zarchini <peymanzarchini@outlook.com>
2024-06-28 21:11:49 +0330

    Creating a user model

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+import { model, models, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
